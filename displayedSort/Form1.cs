c99ad1200b63ff32a8481dbe31a9c5e15ea5214f
vpopi4 <80127175+vpopi4@users.noreply.github.com>
2021-03-27 13:26:15 +0300

    Add files via upload

---
+++ displayedSort/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace displayedSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random r = new Random();
+        int i = 1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 10, y = 0;
+            for (int i = 1; i<= 10; i++)
+            {
+                Controls["PictureBox" + i].Location = new Point(x, y);
+                Controls["PictureBox" + i].Width = 9;
+                Controls["PictureBox" + i].BackColor = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+                Controls["PictureBox" + i].Height = r.Next(10, 300);
+                x += 10;
+            }
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            Control min = Controls["PictureBox" + i];
+
+            for (int j = i; j <= 10; j++)
+            {
+                if (min.Height > Controls["PictureBox" + j].Height)
+                {
+                    int temp1 = Controls["PictureBox" + j].Height;
+                    Controls["PictureBox" + j].Height = min.Height;
+                    min.Height = temp1;
+
+                    Color temp2 = Controls["PictureBox" + j].BackColor;
+                    Controls["PictureBox" + j].BackColor = min.BackColor;
+                    min.BackColor = temp2;
+
+                }
+            }
+            
+            if (i <= 10)
+            {
+                i++;
+                timer1.Start();
+            }
+            
+        }
+    }
+}
---
+++ displayedSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("displayedSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("displayedSort")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c0680b1a-9343-472f-a5d3-95a914156acf")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
